42d566e363eec1dfc21f87f571563d5aaeb5d4ea
caoliveira88 <camilaoliver1@gmail.com>
2022-11-22 08:39:44 -0300

    :tada: First Commit

---
+++ src/desafio_consumo/Comida.java
@@ -0,0 +1,12 @@
+package desafio_consumo;
+
+public class Comida {
+
+    String nome;
+    double peso;
+
+    Comida(String nome, double peso) {
+        this.nome = nome;
+        this.peso = peso;
+    }
+}
---
+++ src/desafio_consumo/Refeicao.java
@@ -0,0 +1,19 @@
+package desafio_consumo;
+
+public class Refeicao {
+
+    public static void main(String[] args) {
+        Comida c1 = new Comida("Arroz", 0.180);
+        Comida c2 = new Comida("Feijão", 0.300);
+
+        Pessoa p1 = new Pessoa("João", 99.8);
+
+        System.out.println(p1.apresentar());
+        p1.comer(c1); // 180 gramas mais pesado
+
+        System.out.println(p1.apresentar());
+        p1.comer(c2); // 200 gramas mais pesado
+
+        System.out.println(p1.apresentar());
+    }
+}
